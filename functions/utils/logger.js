63127371d0d3f74a3b06f89762d6aa25bd4e4a38
Michelangelo Pochì <michelangelo.pochi@gmail.com>
2024-05-31 15:06:12 +0200

    - aggiunta connessione a cloudwatch

--- functions/utils/logger.js
+++ functions/utils/logger.js
@@ -1,5 +1,10 @@
 import winston from 'winston';
+import CloudWatchTransport from 'winston-cloudwatch';
+import dotenv from 'dotenv';
+
 import path from 'path';
+
+dotenv.config();
 
 const logDir = 'log';
 const filename = path.join(logDir, 'results.log');
@@ -8,13 +13,21 @@ export const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
         winston.format.timestamp({
-            format: 'YYYY-MM-DD HH:mm:ss'
+            format: 'DD-MM-YYYY HH:mm:ss'
         }),
-        winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+        winston.format.printf(info => `${info.timestamp} - ${info.level}: ${info.message}`)
     ),
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({ filename })
+        // new winston.transports.File({ filename }),
+        new CloudWatchTransport({
+            logGroupName: 'duneuprisingcompanionapp',  // Sostituisci con il tuo Log Group
+            logStreamName: 'duneuprisingcompanionapp-log',  // Sostituisci con il tuo Log Stream
+            awsRegion: 'eu-west-3',  // Sostituisci con la tua regione AWS
+            awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,  // Sostituisci con il tuo Access Key ID
+            awsSecretKey: process.env.AWS_ACCESS_KEY_SECRET,  // Sostituisci con il tuo Secret Access Key
+            jsonMessage: true
+        })
     ]
 });
 
