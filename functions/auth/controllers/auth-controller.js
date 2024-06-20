d11fea87dec68b6b1f914a4009989215a40fd0ad
Michelangelo Pochì <michelangelo.pochi@gmail.com>
2024-06-20 10:24:02 +0200

    - aggiunto doppio link recupero password

--- functions/auth/controllers/auth-controller.js
+++ functions/auth/controllers/auth-controller.js
@@ -87,6 +87,10 @@ export async function forgotPassword(req, res, next) {
             text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
             Please click on the following link, or paste this into your browser to complete the process:\n
 
+            (for Andoird)\n
+            https://app.duneuprisingcompanion.it/auth/reset-password?token=${token}\n\n
+
+            (for iOS and PWA)\n
             https://pwa.duneuprisingcompanion.it/auth/reset-password?token=${token}\n\n
 
             If you did not request this, please ignore this email and your password will remain unchanged.`,
